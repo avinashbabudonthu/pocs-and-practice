85edabd36e080f93a74924d8cde03efa89361160
Donthu Avinash Babu <avinashbabu.donthu@gmail.com>
2025-01-28 21:10:28 +0530

    [feat]: map struct poc

---
+++ map-struct/src/main/java/com/java/date/format/DateFormatTest.java
@@ -0,0 +1,31 @@
+package com.java.date.format;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class DateFormatTest {
+
+    public static void main(String[] args) {
+        StudentEntity studentEntity1 = new StudentEntity();
+        studentEntity1.setName("a");
+        studentEntity1.setCourse("c1");
+        studentEntity1.setJoiningDate1(new Date());
+        studentEntity1.setJoiningDate2(new GregorianCalendar());
+
+        StudentModel studentModel = StudentMapper.INSTANCE.entityToModel(studentEntity1);
+        String result = String.format("studentModel = %s", studentModel);
+        System.out.println(result);
+
+        StudentEntity studentEntity2 = StudentMapper.INSTANCE.modelToEntity(studentModel);
+        String result2 = String.format("studentEntity2 = %s", studentEntity2);
+        System.out.println(result2);
+    }
+
+    @Test
+    void test1() {
+        Date date = new Date();
+        System.out.println(date);
+    }
+}
