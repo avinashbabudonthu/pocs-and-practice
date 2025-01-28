dccd8acd1632394b692f6dc5cfeaad929ce3bcbe
Donthu Avinash Babu <avinashbabu.donthu@gmail.com>
2025-01-28 21:23:35 +0530

    java

--- map-struct/src/main/java/com/java/date/format/DateFormatTest.java
+++ map-struct/src/main/java/com/java/date/format/DateFormatTest.java
@@ -1,10 +1,12 @@
 package com.java.date.format;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+@Slf4j
 public class DateFormatTest {
 
     public static void main(String[] args) {
@@ -21,6 +23,7 @@ public class DateFormatTest {
         StudentEntity studentEntity2 = StudentMapper.INSTANCE.modelToEntity(studentModel);
         String result2 = String.format("studentEntity2 = %s", studentEntity2);
         System.out.println(result2);
+        log.info("{}", result2);
     }
 
     @Test
