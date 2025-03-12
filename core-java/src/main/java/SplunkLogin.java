09e62f961e82057a8073b2b775828e6042e0ea26
Donthu Avinash Babu <avinashbabu.donthu@gmail.com>
2025-03-12 20:06:03 +0530

    java

--- core-java/src/main/java/SplunkLogin.java
+++ core-java/src/main/java/SplunkLogin.java
@@ -11,12 +11,8 @@ public class SplunkLogin {
         ServiceArgs loginArgs = new ServiceArgs();
         loginArgs.setPort(8089);
         // loginArgs.setHost("localhost");
-//        loginArgs.setHost("https://splunkcdl.es.ad.adp.com/en-US/app/myadp_search/search");
-        loginArgs.setHost("splunkcdl.es.ad.adp.com");
 //        loginArgs.setScheme("https");
         loginArgs.setScheme("http");
-        loginArgs.setUsername("donthuav"); // Use your username
-        loginArgs.setPassword("Password@01012025"); // Use your password
 
         // Initialize the SDK client
         service = Service.connect(loginArgs);
